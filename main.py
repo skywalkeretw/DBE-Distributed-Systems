f092ee7c288eee90f7d33d293d7846a6676a67e5
Luke Roy <lukeroy@Lukes-MacBook-Pro.local>
2022-12-04 11:43:24 +0100

    Working broadcast

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ import socket
 from threading import Thread
 from multiprocessing import Process
 import os
+from uuid import uuid4
+import json
 
 hostname=socket.gethostname()
 IPAddr=socket.gethostbyname(socket.gethostname()+'.')
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = IPAddr #'127.0.0.1'
 client_address = '192.168.178.30'
 server_port = 10001
-
 buffer_size = 4069
+client_id = str(uuid4())
+participants = []
 
+def encode_data(d):
+    return json.dumps(d).encode('utf-8')
+def decode_data(d):
+    json.loads(d.decode('utf-8'))
 
 def receive_messages():
     server_socket.bind((server_address, server_port))
@@ -24,22 +32,40 @@ def receive_messages():
         data, address = server_socket.recvfrom(buffer_size)
         print(f"{address[0]}: {data.decode()}" )
 
-        if data:
-            messge = 'Hi client nice to connect ot you'
-            server_socket.sendto(str.encode(messge), address)
-            print("Replied:", messge)
+def send_messages():
+    while True:
+        msg = input()
+        for participant in participants:
+            client_socket.sendto(msg.encode(), (participant["ip"], server_port))
 
+# echo -n "test data" | nc -u -b 255.255.255.255 10001
+def join_chat():
+    data = {
+        "id": client_id,
+        "ip": IPAddr
+    }
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    #s.sendto(str.encode(IPAddr), ("192.168.178.255", server_port))
+    broadcast_socket.sendto(encode_data(data), ("255.255.255.255", server_port))
 
-
+# nc -luk 10001
+def listen_for_participants():
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.bind(('',server_port))
+    while True:
+        new_participant, address = broadcast_socket.recvfrom(buffer_size)
+        new_participant = decode_data(new_participant)
+        participants.append(new_participant)
 
 
 if __name__ == '__main__':
-    Thread(target=receive_messages).start()
+    #join_chat()
+
+    Thread(target=listen_for_participants).start()
+    # Thread(target=receive_messages).start()
+    # Thread(target=send_messages).start
     print("server Running")
 
-    print("Your Computer Name is:"+hostname)
-    print("Your Computer IP Address is:"+IPAddr)
+    print("Your Computer Name is: "+hostname)
+    print("Your Computer IP Address is: "+IPAddr)
 
-    while True:
-        msg = input()
-        client_socket.sendto(msg.encode(), (client_address, server_port))
