fa9ff7dacba7681111f420a8863d87dc2e209f08
Luke Roy <lukeroy@Lukes-MacBook-Pro-3.local>
2023-01-11 21:39:48 +0100

    join  + uuid + leader

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ import time
 hostname=socket.gethostname()
 IPAddr=socket.gethostbyname(socket.gethostname()+'.')
 buffer_size = 4069
+is_leader = False
+my_uuid = ""
+
+participants_ring = [] # list of ids: 33c76fe6-5c39-46a4-885c-1b770a6e786e
+participants_list = {} # dictionary containing data about the participants
 
 # ports
 broadcast_port = 10001
@@ -27,10 +32,6 @@ chat_port = 10002
 info_server_socket, info_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) , socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 info_port = 10003
 
-
-my_id = ""
-participants_ring = [] # list of ids: 33c76fe6-5c39-46a4-885c-1b770a6e786e
-participants_list = {} # dictionary containing data about the participants
 
 #-------------------------------------------------------------------------------------------------------
 
@@ -95,7 +96,7 @@ def join_chat():
     try:
         print("Listen for uuid")
         confirm_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #confirm_server_socket.settimeout(30)
+        confirm_server_socket.settimeout(5)
         confirm_server_socket.bind((IPAddr, connect_port))
         print("bind successfull")
         confirm_server_socket.listen()
@@ -107,11 +108,8 @@ def join_chat():
         print(f"Connected by {addr}")
         data = conn.recv(buffer_size)
         print(decode_data(data))
-        # if not data:
-        #     count += 1
-        #     time.sleep(1)
-        #     print(count)
-        # else:
+    except socket.error:
+        print("Timeout !!!")
         
 
     finally:
@@ -125,22 +123,23 @@ def listen_for_participants():
         broadcast_server_socket.bind(("255.255.255.255", broadcast_port))
         print()
         while True:
-            try:
-                data, address = broadcast_server_socket.recvfrom(buffer_size)
-                if address[0] != IPAddr:
+            if is_leader:
+                try:
+                    data, address = broadcast_server_socket.recvfrom(buffer_size)
+                    if address[0] != IPAddr:
 
-                    if address:
-                        try:
-                            confirm_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                            confirm_client_socket.connect((address[0], connect_port))
-                            print("connect succesfull")
-                            confirm_client_socket.send(encode_data({"uuid": str(uuid4())}))
-                        finally:
-                            confirm_client_socket.close()
+                        if address:
+                            try:
+                                confirm_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                confirm_client_socket.connect((address[0], connect_port))
+                                print("connect succesfull")
+                                confirm_client_socket.send(encode_data({"uuid": str(uuid4())}))
+                            finally:
+                                confirm_client_socket.close()
 
-            except socket.error:
-                print("Error Occured.") 
-                break
+                except socket.error:
+                    print("Error Occured.") 
+                    break
 
     except socket.error:
         print("Error")
@@ -155,8 +154,7 @@ if __name__ == '__main__':
     print("Your Computer Name is: "+hostname)
     print("Your Computer IP Address is: "+IPAddr)
     # participants.append(participant_data)
+    join_chat()
     
     t_listen_for_participants = Thread(target=listen_for_participants, args=(), daemon=False)
     t_listen_for_participants.start()
-
-    join_chat()
